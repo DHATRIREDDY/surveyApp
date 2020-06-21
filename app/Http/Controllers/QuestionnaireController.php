e5cba645e352e73acbef8f1449ee830d8020d7f2
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-06-21 22:11:30 +0530

    part3

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -29,6 +29,7 @@ class QuestionnaireController extends Controller
 
     public function show(\App\Questionnaire $questionnaire)
     {
+        $questionnaire->load('questions.answers');
       return view('questionnaire.show',compact('questionnaire'));
     }
 }
